685e49c216c3764c3ad68d58c3bbd0049121ee84
Andrew J Said <andrewjsaid@gmail.com>
2018-07-18 14:21:30 +0100

    Add 50% constructor to BooleanVariable

--- src/WillyNilly/BooleanVariable.cs
+++ src/WillyNilly/BooleanVariable.cs
@@ -1,10 +1,10 @@
-﻿using System;
-
-namespace WillyNilly
+﻿namespace WillyNilly
 {
     public class BooleanVariable : IRandomVariable<bool>
     {
         private readonly double _prob;
+
+        public BooleanVariable() : this(0.5) { }
 
         public BooleanVariable(double prob)
         {
